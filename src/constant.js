0bca827d4aeb27f8888465bfacb300d1b109217f
divyannarwani <divyannarwani14@gmail.com>
2024-12-20 10:32:59 +0530

    url change training

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 const PROD_URL = "https://itmagic-backend.vercel.app/api/v1"
 const LOCAL_URL = "http://localhost:8000/api/v1"
-const BASE_URL = PROD_URL;
+const BASE_URL = LOCAL_URL;
 
 export default BASE_URL;
