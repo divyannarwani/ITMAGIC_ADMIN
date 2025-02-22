140fc2fc9c51aa82be47cf57a6ad884092922f66
divyannarwani <divyannarwani14@gmail.com>
2025-02-22 13:25:20 +0530

    Render link updated

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,6 @@
 const PROD_URL = "https://itmagic-backend.vercel.app/api/v1"
+const PROD_URL_render = "https://itmagic-backend.onrender.com/api/v1"
 const LOCAL_URL = "http://localhost:8000/api/v1"
-const BASE_URL = PROD_URL;
+const BASE_URL = PROD_URL_render;
 
 export default BASE_URL;
